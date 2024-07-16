89b3072063e688c5e4ec7aa0dd46213b5ba1c3a0
roylene17 <rensyd75@gmail.com>
2024-07-16 19:12:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const app=express()
 const port=5000;
-//post is the end point
-//app.use('/myapi',(req,res)=>{
+
+app.use('/myapi',(req,res)=>{
     res.send("Hello,World!");
-//})//if it doesn't work then terminal ctrl+c /then run prgm/ then localhost
+})
 
 app.listen(port,()=>{
   console.log("server listening to 5000")
